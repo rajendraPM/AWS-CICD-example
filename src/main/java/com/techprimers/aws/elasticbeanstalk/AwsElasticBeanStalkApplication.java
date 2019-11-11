bcc54b264ab45f2dba2d702d97fd7f3f3eb4db37
Rajendra Menega <rajendra.menega@gmail.com>
2019-11-12 05:53:07 +0800

    initial code base

---
+++ src/main/java/com/techprimers/aws/elasticbeanstalk/AwsElasticBeanStalkApplication.java
@@ -0,0 +1,13 @@
+package com.techprimers.aws.elasticbeanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanStalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanStalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techprimers/aws/elasticbeanstalk/AwsElasticBeanStalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.aws.elasticbeanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanStalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
